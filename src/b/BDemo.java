59bcf5345e9e02a26c18d781bcd32bf30f7430ca
lenovo <lenovo@LAPTOP-MJ4J3446>
2025-02-02 18:19:19 -0600

    adding divide method

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -5,5 +5,9 @@ public class BDemo {
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamD");
 	}
+	
+	public void divide(int a, int b) {
+		System.out.println(a/b);
+	}
 
 }
